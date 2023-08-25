e45cfe90a5c22cce466725dbd7bcd33e51259c77
Albin Antony <albin.pa3@gmail.com>
2023-08-25 18:46:04 +0530

    Add #129 Update data attribute by ID

--- src/handler/organization_handler.go
+++ src/handler/organization_handler.go
@@ -1085,6 +1085,74 @@ func GetTemplateByID(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
+// UpdateTemplateByID Updates an organization consent templates
+func UpdateTemplateByID(w http.ResponseWriter, r *http.Request) {
+	organizationID := mux.Vars(r)["organizationID"]
+	templateID := mux.Vars(r)["templateID"]
+
+	var uReq template
+	b, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	json.Unmarshal(b, &uReq)
+
+	o, err := org.Get(organizationID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to fetch organization: %v", organizationID)
+		common.HandleError(w, http.StatusInternalServerError, m, err)
+		return
+	}
+
+	// validating PurposeIDs provided
+	if uReq.PurposeIDs != nil {
+
+		for _, p := range uReq.PurposeIDs {
+			_, err := org.GetPurpose(organizationID, p)
+			if err != nil {
+				m := fmt.Sprintf("Invalid purposeID:%v provided;Failed to update template to organization: %v", p, o.Name)
+				common.HandleError(w, http.StatusBadRequest, m, err)
+				return
+			}
+		}
+	}
+
+	var found = false
+
+	for i := range o.Templates {
+		if o.Templates[i].ID == templateID {
+			found = true
+
+			// for partial updation
+			if strings.TrimSpace(uReq.Consent) != "" {
+				o.Templates[i].Consent = uReq.Consent
+			}
+			// only updating if any valid purpose id was given
+			if uReq.PurposeIDs != nil {
+				o.Templates[i].PurposeIDs = uReq.PurposeIDs
+			}
+		}
+	}
+
+	if !found {
+		m := fmt.Sprintf("Failed to find template with ID: %v in organization: %v", templateID, o.Name)
+		common.HandleError(w, http.StatusNotFound, m, err)
+		return
+	}
+
+	o, err = org.Update(o)
+	if err != nil {
+		m := fmt.Sprintf("Failed to update template: %v in organization: %v", templateID, o.Name)
+		common.HandleError(w, http.StatusNotFound, m, err)
+		return
+	}
+
+	response, _ := json.Marshal(organization{o})
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
+
 func removeElementFromSlice(slice []string, index int) []string {
 
 	newSlice := make([]string, 0)
