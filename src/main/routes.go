240088676e75639c417f2b241a5e55c2e10e30b1
Albin Antony <albin.pa3@gmail.com>
2023-08-25 10:33:47 +0530

    Add #41 Get organisation by ID

--- src/main/routes.go
+++ src/main/routes.go
@@ -19,6 +19,7 @@ func SetRoutes(r *mux.Router) {
 	r.HandleFunc("/", healthz).Methods("GET")
 
 	r.Handle("/v1/organizations", m.Chain(handler.AddOrganization, m.Logger(), m.Authenticate())).Methods("POST")
+	r.Handle("/v1/organizations/{organizationID}", m.Chain(handler.GetOrganizationByID, m.Logger(), m.Authenticate())).Methods("GET")
 
 	//Login
 	r.Handle("/v1/users/register", m.Chain(handler.RegisterUser, m.LoggerNoAuth())).Methods("POST")
