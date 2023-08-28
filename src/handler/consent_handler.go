4fe36e992ef4677960c0d49d4e087ccb80fe9b5d
Albin Antony <albin.pa3@gmail.com>
2023-08-28 10:31:59 +0530

    Add #85 Get consent purpose by ID

--- src/handler/consent_handler.go
+++ src/handler/consent_handler.go
@@ -212,3 +212,85 @@ func GetConsentByID(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(response)
 }
+
+type consentPurposeResp struct {
+	ID            string
+	ConsentID     string
+	OrgID         string
+	UserID        string
+	Consents      consentsAndPurpose
+	DataRetention DataRetentionPolicyResp
+}
+
+// GetConsentPurposeByID Gets all the consents for agiven purpose by ID
+func GetConsentPurposeByID(w http.ResponseWriter, r *http.Request) {
+	orgID := mux.Vars(r)["orgID"]
+	userID := mux.Vars(r)["userID"]
+	consentID := mux.Vars(r)["consentID"]
+	purposeID := mux.Vars(r)["purposeID"]
+
+	o, err := org.Get(orgID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to fetch organization for user: %v org: %v", userID, orgID)
+		common.HandleError(w, http.StatusInternalServerError, m, err)
+		return
+	}
+
+	consent, err := consent.Get(consentID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to fetch consensts by ID: %v for user: %v", consentID, userID)
+		common.HandleError(w, http.StatusNotFound, m, err)
+		return
+	}
+
+	c := createConsentGetResponse(consent, o)
+
+	var found = 0
+	var cp consentsAndPurpose
+	for _, item := range c.ConsentsAndPurposes {
+		if item.Purpose.ID == purposeID {
+			cp = item
+			found++
+		}
+	}
+	if found == 0 {
+		//TODO: Handle the case where the purpose ID is non existent
+	}
+
+	var cpResp consentPurposeResp
+	cpResp.ID = purposeID
+	cpResp.ConsentID = consentID
+	cpResp.OrgID = orgID
+	cpResp.UserID = userID
+	cpResp.Consents = cp
+
+	// Data retention expiry
+	if o.DataRetention.Enabled {
+
+		// Check if atleast one attribute consent is allowed
+		isPurposeAllowed := false
+		for _, attributeConsent := range cpResp.Consents.Consents {
+			if attributeConsent.Status.Consented == common.ConsentStatusAllow {
+				isPurposeAllowed = true
+				break
+			}
+		}
+
+		if isPurposeAllowed {
+
+			latestConsentHistory, err := consenthistory.GetLatestByUserOrgPurposeID(userID, orgID, purposeID)
+			if err != nil {
+				response, _ := json.Marshal(cpResp)
+				w.Header().Set("Content-Type", "application/json")
+				w.Write(response)
+				return
+			}
+
+			cpResp.DataRetention.Expiry = latestConsentHistory.ID.Time().Add(time.Second * time.Duration(o.DataRetention.RetentionPeriod)).UTC().String()
+		}
+	}
+
+	response, _ := json.Marshal(cpResp)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
