74219c07563f9b00899d64a42c5198fdd47b400f
Albin Antony <albin.pa3@gmail.com>
2023-08-28 10:08:57 +0530

    Add #82 Get identity provider

--- src/handler/iam_handler.go
+++ src/handler/iam_handler.go
@@ -1999,3 +1999,28 @@ func DeleteIdentityProvider(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 	w.Write(nil)
 }
+
+// GetIdentityProvider Get external identity provider for an organisation
+func GetIdentityProvider(w http.ResponseWriter, r *http.Request) {
+
+	// Get the org ID and fetch the organization from the db.
+	organizationID := mux.Vars(r)["organizationID"]
+	o, err := org.Get(organizationID)
+
+	if err != nil {
+		m := fmt.Sprintf("Failed to fetch org; Failed to get identity provider for %v", organizationID)
+		common.HandleError(w, http.StatusNotFound, m, err)
+		return
+	}
+
+	if !o.ExternalIdentityProviderAvailable {
+		m := fmt.Sprintf("External IDP provider doesn't exist; Try to create instead of get; Failed to get identity provider for %v", organizationID)
+		common.HandleError(w, http.StatusNotFound, m, err)
+		return
+	}
+
+	response, _ := json.Marshal(o.IdentityProviderRepresentation.Config)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
