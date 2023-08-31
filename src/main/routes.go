69d98b8a31e36f24af8480abf9804921aaa570a1
Albin Antony <albin.pa3@gmail.com>
2023-08-31 16:22:59 +0530

    Add #147 Get all download my data requests

--- src/main/routes.go
+++ src/main/routes.go
@@ -152,4 +152,6 @@ func SetRoutes(r *mux.Router) {
 	r.Handle("/v1/user/organizations/{orgID}/data-delete/status", m.Chain(handler.GetDeleteMyDataStatus, m.Logger(), m.Authenticate())).Methods("GET")
 	r.Handle("/v1/user/organizations/{orgID}/data-delete/{dataReqID}/cancel", m.Chain(handler.CancelMyDataRequest, m.Logger(), m.Authenticate())).Methods("POST")
 
+	r.Handle("/v1/user/organizations/{orgID}/data-download", m.Chain(handler.GetDownloadMyData, m.Logger(), m.Authenticate())).Methods("GET")
+
 }
