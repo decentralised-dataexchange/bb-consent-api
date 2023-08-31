0b7ed0ae51ae644308b7e4e19043bf9ce9b6f239
Albin Antony <albin.pa3@gmail.com>
2023-08-31 11:23:43 +0530

    Add #110 Re-deliver a webhook payload by delivery ID

--- src/main/routes.go
+++ src/main/routes.go
@@ -108,6 +108,7 @@ func SetRoutes(r *mux.Router) {
 	r.Handle("/v1/organizations/{orgID}/webhooks/{webhookID}/ping", m.Chain(handler.PingWebhook, m.Logger(), m.Authenticate())).Methods("POST")
 	r.Handle("/v1/organizations/{orgID}/webhooks/{webhookID}/delivery", m.Chain(handler.GetRecentWebhookDeliveries, m.Logger(), m.Authenticate())).Methods("GET")
 	r.Handle("/v1/organizations/{orgID}/webhooks/{webhookID}/delivery/{deliveryID}", m.Chain(handler.GetWebhookDeliveryByID, m.Logger(), m.Authenticate())).Methods("GET")
+	r.Handle("/v1/organizations/{orgID}/webhooks/{webhookID}/delivery/{deliveryID}/redeliver", m.Chain(handler.ReDeliverWebhook, m.Logger(), m.Authenticate())).Methods("POST")
 
 	//Login
 	r.Handle("/v1/users/register", m.Chain(handler.RegisterUser, m.LoggerNoAuth())).Methods("POST")
