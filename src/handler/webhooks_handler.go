d7832501db8c3c62b5ed1718e137ec258b5c2769
Albin Antony <albin.pa3@gmail.com>
2023-08-31 11:21:54 +0530

    Add #109 Get recent webhook delivery by ID

--- src/handler/webhooks_handler.go
+++ src/handler/webhooks_handler.go
@@ -619,3 +619,68 @@ func GetRecentWebhookDeliveries(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 
 }
+
+type webhookDeliveryResp struct {
+	ID                      bson.ObjectId       `bson:"_id,omitempty"`
+	RequestHeaders          map[string][]string // HTTP headers posted to webhook endpoint
+	RequestPayload          interface{}         // JSON payload posted to webhook endpoint
+	ResponseHeaders         map[string][]string // HTTP response headers received from webhook endpoint
+	ResponseBody            string              // HTTP response body received from webhook endpoint in bytes
+	ResponseStatusCode      int                 // HTTP response status code
+	ResponseStatusStr       string              // HTTP response status string
+	ExecutionStartTimeStamp string              // UTC timestamp when webhook execution started
+	ExecutionEndTimeStamp   string              // UTC timestamp when webhook execution ended
+	Status                  string              // Status of webhook delivery for e.g. failed or completed
+	StatusDescription       string              // Describe the status for e.g. Reason for failure
+}
+
+// GetWebhookDeliveryByID Gets the payload delivery details for a webhook by ID
+func GetWebhookDeliveryByID(w http.ResponseWriter, r *http.Request) {
+	// Reading the URL parameters
+	organizationID := mux.Vars(r)["orgID"]
+	webhookID := mux.Vars(r)["webhookID"]
+	deliveryID := mux.Vars(r)["deliveryID"]
+
+	// Validating the given organisation ID
+	_, err := org.Get(organizationID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to get organization: %v", organizationID)
+		common.HandleError(w, http.StatusBadRequest, m, err)
+		return
+	}
+
+	// Validating the given webhook ID for an organisation
+	webhook, err := wh.GetByOrgID(webhookID, organizationID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to get webhook:%v for organisation: %v", webhookID, organizationID)
+		common.HandleError(w, http.StatusBadRequest, m, err)
+		return
+	}
+
+	// Get the webhook delivery by ID
+	webhookDelivery, err := wh.GetWebhookDeliveryByID(webhook.ID.Hex(), deliveryID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to get delivery details by ID:%v for webhook:%v for organisation: %v", deliveryID, webhookID, organizationID)
+		common.HandleError(w, http.StatusBadRequest, m, err)
+		return
+	}
+
+	resp := webhookDeliveryResp{
+		ID:                      webhookDelivery.ID,
+		RequestHeaders:          webhookDelivery.RequestHeaders,
+		RequestPayload:          webhookDelivery.RequestPayload,
+		ResponseHeaders:         webhookDelivery.ResponseHeaders,
+		ResponseBody:            webhookDelivery.ResponseBody,
+		ResponseStatusCode:      webhookDelivery.ResponseStatusCode,
+		ResponseStatusStr:       webhookDelivery.ResponseStatusStr,
+		ExecutionStartTimeStamp: webhookDelivery.ExecutionStartTimeStamp,
+		ExecutionEndTimeStamp:   webhookDelivery.ExecutionEndTimeStamp,
+		Status:                  webhookDelivery.Status,
+		StatusDescription:       webhookDelivery.StatusDescription,
+	}
+
+	response, _ := json.Marshal(resp)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
