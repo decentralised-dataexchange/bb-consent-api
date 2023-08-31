ebb516858a39c4e2c0bedeec4026a169a5b4e705
Albin Antony <albin.pa3@gmail.com>
2023-08-31 11:23:43 +0530

    Add #110 Re-deliver a webhook payload by delivery ID

--- src/handler/webhooks_handler.go
+++ src/handler/webhooks_handler.go
@@ -11,8 +11,10 @@ import (
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/bb-consent/api/src/actionlog"
 	"github.com/bb-consent/api/src/common"
 	"github.com/bb-consent/api/src/org"
+	"github.com/bb-consent/api/src/user"
 	wh "github.com/bb-consent/api/src/webhooks"
 	"github.com/globalsign/mgo/bson"
 	"github.com/gorilla/mux"
@@ -683,4 +685,62 @@ func GetWebhookDeliveryByID(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
+}
+
+// ReDeliverWebhook Redo payload delivery to the webhook
+func ReDeliverWebhook(w http.ResponseWriter, r *http.Request) {
+	// Reading the URL parameters
+	organizationID := mux.Vars(r)["orgID"]
+	webhookID := mux.Vars(r)["webhookID"]
+	deliveryID := mux.Vars(r)["deliveryID"]
+
+	// Validating the given organisation ID
+	_, err := org.Get(organizationID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to get organization: %v", organizationID)
+		common.HandleError(w, http.StatusBadRequest, m, err)
+		return
+	}
+
+	// Validating the given webhook ID for an organisation
+	webhook, err := wh.GetByOrgID(webhookID, organizationID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to get webhook:%v for organisation: %v", webhookID, organizationID)
+		common.HandleError(w, http.StatusBadRequest, m, err)
+		return
+	}
+
+	// Validating the given delivery ID for a webhook
+	webhookDelivery, err := wh.GetWebhookDeliveryByID(webhook.ID.Hex(), deliveryID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to get delivery details by ID:%v for webhook:%v for organisation: %v", deliveryID, webhookID, organizationID)
+		common.HandleError(w, http.StatusBadRequest, m, err)
+		return
+	}
+
+	// Converting the webhook payload to bytes
+	webhookPayloadBytes, err := json.Marshal(webhookDelivery.RequestPayload)
+	if err != nil {
+		m := fmt.Sprintf("Failed to convert webhook event data to bytes, error:%v; Failed to redeliver payload for webhook for event:<%s>, user:<%s>, org:<%s>", err.Error(), webhookDelivery.WebhookEventType, webhookDelivery.UserID, organizationID)
+		common.HandleError(w, http.StatusInternalServerError, m, err)
+		return
+	}
+
+	// Get the details of who triggered webhook
+	u, err := user.Get(webhookDelivery.UserID)
+	if err != nil {
+		m := fmt.Sprintf("Failed to get user, error:%v; Failed to redeliver payload for webhook for event:<%s>, user:<%s>, org:<%s>", err.Error(), webhookDelivery.WebhookEventType, webhookDelivery.UserID, organizationID)
+		common.HandleError(w, http.StatusInternalServerError, m, err)
+		return
+	}
+
+	go wh.PushWebhookEventToKafkaTopic(webhookDelivery.WebhookEventType, webhookPayloadBytes, wh.WebhooksConfiguration.KafkaConfig.Topic)
+
+	// Log webhook calls in webhooks category
+	aLog := fmt.Sprintf("Organization webhook: %v triggered by user: %v by event: %v", webhook.PayloadURL, u.Email, webhookDelivery.WebhookEventType)
+	actionlog.LogOrgWebhookCalls(u.ID.Hex(), u.Email, organizationID, aLog)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
 }
