04ff63833fb7bf671c3ac84200abb229457dded8
Albin Antony <albin.pa3@gmail.com>
2023-08-31 16:16:48 +0530

    Add #145 Get delete my data request status

--- src/main/routes.go
+++ src/main/routes.go
@@ -149,5 +149,6 @@ func SetRoutes(r *mux.Router) {
 
 	r.Handle("/v1/user/organizations/{orgID}/data-delete", m.Chain(handler.GetDeleteMyData, m.Logger(), m.Authenticate())).Methods("GET")
 	r.Handle("/v1/user/organizations/{orgID}/data-delete", m.Chain(handler.DeleteMyData, m.Logger(), m.Authenticate())).Methods("POST")
+	r.Handle("/v1/user/organizations/{orgID}/data-delete/status", m.Chain(handler.GetDeleteMyDataStatus, m.Logger(), m.Authenticate())).Methods("GET")
 
 }
