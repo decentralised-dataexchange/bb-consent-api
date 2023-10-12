548e9af1ca5cb2bbe6644a74004e58bdde7cd13a
Albin Antony <albin.pa3@gmail.com>
2023-10-12 13:54:02 +0530

    Fix #221 Add middleware to add response header - Content-Type

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -237,3 +237,18 @@ func SetApplicationMode() Middleware {
 		}
 	}
 }
+
+func AddContentType() Middleware {
+	// Create a new Middleware
+	return func(f http.HandlerFunc) http.HandlerFunc {
+
+		// Define the http.HandlerFunc
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			w.Header().Add(config.ContentTypeHeader, config.ContentTypeJSON)
+
+			// Call the next middleware/handler in chain
+			f(w, r)
+		}
+	}
+}
