dbc6b32740c5f032d0f0f8a32fdf4449789e35b2
Albin Antony <albin.pa3@gmail.com>
2023-10-18 22:19:16 +0530

    Add #274 Process webhooks in goroutine
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

---
+++ src/v2/webhook_dispatcher/db.go
@@ -0,0 +1,38 @@
+package webhook_dispatcher
+
+import (
+	"context"
+
+	"github.com/bb-consent/api/src/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func webhookCollection() *mongo.Collection {
+	return database.DB.Client.Database(database.DB.Name).Collection("webhooks")
+}
+
+func webhookDeliveryCollection() *mongo.Collection {
+	return database.DB.Client.Database(database.DB.Name).Collection("webhookDeliveries")
+}
+
+// GetWebhookByOrgID Gets a webhook by organisation ID and webhook ID
+func GetWebhookByOrgID(webhookId, orgID string) (result Webhook, err error) {
+
+	err = webhookCollection().FindOne(context.TODO(), bson.M{"_id": webhookId, "orgid": orgID}).Decode(&result)
+
+	return result, err
+}
+
+// AddWebhookDelivery Adds payload delivery details to database for a webhook event
+func AddWebhookDelivery(webhookDelivery WebhookDelivery) (WebhookDelivery, error) {
+
+	if webhookDelivery.ID == primitive.NilObjectID {
+		webhookDelivery.ID = primitive.NewObjectID()
+	}
+
+	_, err := webhookDeliveryCollection().InsertOne(context.TODO(), &webhookDelivery)
+
+	return webhookDelivery, err
+}
