409fc670c1630640aa9b916540cad3167d9f9788
Albin Antony <albin.pa3@gmail.com>
2023-10-19 00:11:15 +0530

    Add #271 Keycloak: Migrate from homebrewed API calls to gocloak

--- go.mod
+++ go.mod
@@ -28,7 +28,10 @@ require (
 	cloud.google.com/go/storage v1.33.0 // indirect
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/Knetic/govaluate v3.0.1-0.20171022003610-9aa49832a739+incompatible // indirect
+	github.com/Nerzal/gocloak/v13 v13.8.0 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
@@ -41,6 +44,9 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/sridharv/fakegopath v0.0.0-20160915143632-09e1db74c7e5 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
--- src/v2/handler/individual/config_delete_user.go
+++ src/v2/handler/individual/config_delete_user.go
@@ -1,13 +1,13 @@
 package individual
 
 import (
+	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/Nerzal/gocloak/v13"
 	"github.com/bb-consent/api/src/common"
 	"github.com/bb-consent/api/src/config"
 	"github.com/bb-consent/api/src/v2/individual"
@@ -15,40 +15,9 @@ import (
 )
 
 // unregisterUser Unregisters an existing user
-func unregisterUser(iamUserID string, adminToken string) (int, iamError, error) {
-	var e iamError
-	var status = http.StatusInternalServerError
-	req, err := http.NewRequest("DELETE", iamConfig.URL+"/admin/realms/"+iamConfig.Realm+"/users/"+iamUserID, nil)
-	if err != nil {
-		return status, e, err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+adminToken)
-	req.Header.Add(config.ContentTypeHeader, config.ContentTypeJSON)
-
-	client := http.Client{
-		Timeout: timeout,
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		return status, e, err
-	}
-
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusNoContent {
-		body, _ := ioutil.ReadAll(resp.Body)
-		defer resp.Body.Close()
-
-		type errorMsg struct {
-			ErrorMessage string `json:"errorMessage"`
-		}
-		var errMsg errorMsg
-		json.Unmarshal(body, &errMsg)
-		e.Error = errMsg.ErrorMessage
-		e.ErrorType = "Creation failed"
-		return resp.StatusCode, e, errors.New("failed to unregister user")
-	}
-	return resp.StatusCode, e, err
+func unregisterUser(iamUserID string, adminToken string, client *gocloak.GoCloak) error {
+	err := client.DeleteUser(context.Background(), adminToken, iamConfig.Realm, iamUserID)
+	return err
 }
 
 type deleteIndividualResp struct {
@@ -74,17 +43,19 @@ func ConfigDeleteIndividual(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	t, status, iamErr, err := getAdminToken()
+	client := getClient()
+
+	t, err := getAdminToken(client)
 	if err != nil {
 		log.Printf("Failed to get admin token, user: %v registration", individualId)
-		handleError(w, individualId, status, iamErr, err)
+		common.HandleErrorV2(w, http.StatusBadRequest, err.Error(), err)
 		return
 	}
 
-	status, iamErr, err = unregisterUser(individual.IamId, t.AccessToken)
+	err = unregisterUser(individual.IamId, t.AccessToken, client)
 	if err != nil {
-		log.Printf("Failed to unregister user: %v err: %v", individualId, err)
-		handleError(w, individualId, status, iamErr, err)
+		m := fmt.Sprintf("Failed to unregister user: %v err: %v", individualId, err)
+		common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
 		return
 	}
 
