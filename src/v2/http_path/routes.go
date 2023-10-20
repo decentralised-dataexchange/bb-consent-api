91667d78091d91afbdf45c6ab6aee1eeb3114f5a
Albin Antony <albin.pa3@gmail.com>
2023-10-21 01:26:04 +0530

    Add #303 Forgot password for consent bb individuals and not for individuals onboarded from idp

--- src/v2/http_path/routes.go
+++ src/v2/http_path/routes.go
@@ -120,6 +120,7 @@ func SetRoutes(r *mux.Router, e *casbin.Enforcer) {
 
 	r.Handle(OnboardRefreshToken, m.Chain(onboardHandler.OnboardRefreshToken, m.AddContentType())).Methods("POST")
 	r.Handle(ExchangeAuthorizationCode, m.Chain(onboardHandler.ExchangeAuthorizationCode, m.LoggerNoAuth())).Methods("POST")
+	r.Handle(OnboardForgotPassword, m.Chain(onboardHandler.OnboardForgotPassword, m.LoggerNoAuth())).Methods("PUT")
 
 	r.Handle(GetOrganizationByID, m.Chain(onboardHandler.OnboardReadOrganisation, m.Logger(), m.Authorize(e), m.SetApplicationMode(), m.Authenticate(), m.AddContentType())).Methods("GET")
 	r.Handle(UpdateOrganization, m.Chain(onboardHandler.UpdateOrganization, m.Logger(), m.Authorize(e), m.SetApplicationMode(), m.Authenticate(), m.AddContentType())).Methods("PUT")
