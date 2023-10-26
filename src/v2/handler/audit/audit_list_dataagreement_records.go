bb099beb960286cf8440c507441dc3d085c70262
Albin Antony <albin.pa3@gmail.com>
2023-10-27 04:56:47 +0530

    Add #387 Update audit list data agreement records to use id field as query param

--- src/v2/handler/audit/audit_list_dataagreement_records.go
+++ src/v2/handler/audit/audit_list_dataagreement_records.go
@@ -1,7 +1,7 @@
 package audit
 
 import (
-	"encoding/json"
+	"context"
 	"errors"
 	"log"
 	"net/http"
@@ -10,15 +10,8 @@ import (
 	"github.com/bb-consent/api/src/config"
 	daRecord "github.com/bb-consent/api/src/v2/dataagreement_record"
 	"github.com/bb-consent/api/src/v2/paginate"
+	"go.mongodb.org/mongo-driver/bson"
 )
-
-func dataAgreementRecordToInterfaceSlice(dataAgreementRecords []daRecord.DataAgreementRecordForAuditList) []interface{} {
-	interfaceSlice := make([]interface{}, len(dataAgreementRecords))
-	for i, r := range dataAgreementRecords {
-		interfaceSlice[i] = r
-	}
-	return interfaceSlice
-}
 
 type DataAgreementForListDataAgreementRecord struct {
 	Id      string `json:"id" bson:"_id,omitempty"`
@@ -43,119 +36,59 @@ func AuditListDataAgreementRecords(w http.ResponseWriter, r *http.Request) {
 	darRepo := daRecord.DataAgreementRecordRepository{}
 	darRepo.Init(organisationId)
 
-	var isNotDataAgreementRecordId bool
-	var isNotIndividualId bool
-	var isNotDataAgreementId bool
-	var isNotLawfulBasis bool
-	dataAgreementRecordId, err := daRecord.ParseQueryParams(r, config.DataAgreementRecordId, daRecord.DataAgreementRecordIdIsMissingError)
-	if err != nil && errors.Is(err, daRecord.DataAgreementRecordIdIsMissingError) {
-		isNotDataAgreementRecordId = true
-	}
-	dataAgreementId, err := daRecord.ParseQueryParams(r, config.DataAgreementId, daRecord.DataAgreementIdIsMissingError)
-	if err != nil && errors.Is(err, daRecord.DataAgreementIdIsMissingError) {
-		isNotDataAgreementId = true
-	}
-	individualId, err := daRecord.ParseQueryParams(r, config.IndividualId, daRecord.IndividualIdIsMissingError)
-	if err != nil && errors.Is(err, daRecord.IndividualIdIsMissingError) {
-		isNotIndividualId = true
+	var isIdExists bool
+	var isLawfulBasis bool
+	id, err := daRecord.ParseQueryParams(r, config.Id, daRecord.IdIsMissingError)
+	if err != nil && errors.Is(err, daRecord.IdIsMissingError) {
+		isIdExists = false
+	} else {
+		isIdExists = true
 	}
 
 	lawfulBasis, err := daRecord.ParseQueryParams(r, config.LawfulBasis, daRecord.LawfulBasisIsMissingError)
 	if err != nil && errors.Is(err, daRecord.LawfulBasisIsMissingError) {
-		isNotLawfulBasis = true
-	}
-
-	var daRecords []daRecord.DataAgreementRecordForAuditList
-	if isNotLawfulBasis {
-		if isNotDataAgreementRecordId {
-			if isNotIndividualId {
-				if isNotDataAgreementId {
-					// fetch all data agreement records
-					daRecords, err = daRecord.ListsWithDataAgreementAndTimestamp(organisationId)
-					if err != nil {
-						m := "Failed to fetch data agreement records"
-						common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
-						return
-					}
-
-				} else {
-					// fetch by data agreement id
-					daRecords, err = daRecord.ListByDataAgreementIdIncludingDataAgreement(dataAgreementId, organisationId)
-					if err != nil {
-						m := "Failed to fetch data agreement record"
-						common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
-						return
-					}
-				}
-
-			} else {
-				// fetch by individual id
-				daRecords, err = daRecord.ListByIndividualIdIncludingDataAgreement(individualId, organisationId)
-				if err != nil {
-					m := "Failed to fetch data agreement record"
-					common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
-					return
-				}
-			}
-
-		} else {
-			// fetch by data agreement record id
-			daRecords, err = daRecord.ListByIdIncludingDataAgreement(dataAgreementRecordId, organisationId)
-			if err != nil {
-				m := "Failed to fetch data agreement record"
-				common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
-				return
-			}
-
-		}
-
+		isLawfulBasis = false
 	} else {
-		if isNotDataAgreementRecordId {
-			if isNotIndividualId {
-				// fetch by data agreement id and lawful basis
-				daRecords, err = daRecord.ListByDataAgreementIdAndLawfulBasis(dataAgreementId, organisationId, lawfulBasis)
-				if err != nil {
-					m := "Failed to fetch data agreement record"
-					common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
-					return
-				}
-			} else {
-				// fetch by individual id and lawfulusage
-				daRecords, err = daRecord.ListByIndividualIdAndLawfulBasis(individualId, organisationId, lawfulBasis)
-				if err != nil {
-					m := "Failed to fetch data agreement record"
-					common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
-					return
-				}
-			}
+		isLawfulBasis = true
+	}
 
-		} else {
-			// fetch by data agreement record id and lawful basis
-			daRecords, err = daRecord.ListByIdAndLawfulBasis(dataAgreementRecordId, organisationId, lawfulBasis)
-			if err != nil {
-				m := "Failed to fetch data agreement record"
-				common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
-				return
-			}
+	pipeline, err := daRecord.PipelineForList(organisationId, id, lawfulBasis, isIdExists, isLawfulBasis)
+	if err != nil {
+		m := "Failed to create pipeline"
+		common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
+		return
+	}
 
+	pipeline = append(pipeline, bson.M{"$sort": bson.M{"timestamp": -1}})
+	var daRecords []daRecord.DataAgreementRecordForAuditList
+	query := paginate.PaginateDBObjectsQueryUsingPipeline{
+		Pipeline:   pipeline,
+		Collection: daRecord.Collection(),
+		Context:    context.Background(),
+		Limit:      limit,
+		Offset:     offset,
+	}
+	var resp fetchDataAgreementRecordsResp
+	result, err := paginate.PaginateDBObjectsUsingPipeline(query, &daRecords)
+	if err != nil {
+		if errors.Is(err, paginate.EmptyDBError) {
+			emptyDaRecords := make([]interface{}, 0)
+			resp = fetchDataAgreementRecordsResp{
+				DataAgreementRecords: emptyDaRecords,
+				Pagination:           result.Pagination,
+			}
+			common.ReturnHTTPResponse(resp, w)
+			return
 		}
+		m := "Failed to paginate data agreement records"
+		common.HandleErrorV2(w, http.StatusInternalServerError, m, err)
+		return
 
 	}
-	query := paginate.PaginateObjectsQuery{
-		Limit:  limit,
-		Offset: offset,
-	}
-	interfaceSlice := dataAgreementRecordToInterfaceSlice(daRecords)
-	result := paginate.PaginateObjects(query, interfaceSlice)
-
-	var resp = fetchDataAgreementRecordsResp{
+	resp = fetchDataAgreementRecordsResp{
 		DataAgreementRecords: result.Items,
 		Pagination:           result.Pagination,
 	}
-
-	response, _ := json.Marshal(resp)
-	w.Header().Set(config.ContentTypeHeader, config.ContentTypeJSON)
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
+	common.ReturnHTTPResponse(resp, w)
 
 }
