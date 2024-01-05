6c167d7289726fdaffc893d3ee816c801fcc8ca4
Albin Antony <albin.pa3@gmail.com>
2024-01-05 10:21:21 +0530

    Fix #610 Fetch revisions by objectId and schemaName in revision pipeline

--- internal/revision/db.go
+++ internal/revision/db.go
@@ -168,3 +168,35 @@ func GetLatestByObjectId(objectId string) (Revision, error) {
 
 	return result, err
 }
+
+// GetLatestByObjectIdAndSchemaName Gets latest revision by object id and schema name
+func GetLatestByObjectIdAndSchemaName(objectId string, schemaName string) (Revision, error) {
+
+	var result Revision
+	opts := options.FindOne().SetSort(bson.M{"timestamp": -1})
+	err := Collection().FindOne(context.TODO(), bson.M{"objectid": objectId, "schemaname": schemaName}, opts).Decode(&result)
+	if err != nil {
+		return Revision{}, err
+	}
+
+	return result, err
+}
+
+// ListAllByObjectIdAndSchemaName list revisions by object id and schema name
+func ListAllByObjectIdAndSchemaName(objectId string, schemaName string) ([]Revision, error) {
+
+	var results []Revision
+	opts := options.Find().SetSort(bson.M{"timestamp": -1})
+	cursor, err := Collection().Find(context.TODO(), bson.M{"objectid": objectId, "schemaname": schemaName}, opts)
+	if err != nil {
+		return []Revision{}, err
+	}
+
+	defer cursor.Close(context.TODO())
+
+	if err := cursor.All(context.TODO(), &results); err != nil {
+		return []Revision{}, err
+	}
+
+	return results, err
+}
